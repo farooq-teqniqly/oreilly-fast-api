7b8a9a03cf49255411f82d3c1b6a5925ae1fcfc1
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-09-03 09:41:51 -0700

    chore: Add address parameter to Start-Server function

--- scripts.ps1
+++ scripts.ps1
@@ -1,13 +1,16 @@
 function Start-Server {
     param (
         [string]$App = "hello_world:app",
+        [string]$Address = "127.0.0.1",
         [switch]$Reload
     )
 
     $uvicornArgs = @($App)
+    $uvicornArgs += "--host $Address"
+
     if ($Reload) {
         $uvicornArgs += "--reload"
     }
 
-    & uvicorn @uvicornArgs
+    uvicorn @uvicornArgs
 }
