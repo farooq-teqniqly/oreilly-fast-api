9014d3a7ff488dbd40e82fd8c16e2cffea388ba4
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-09-04 10:32:01 -0700

    refactor: Handle invalid Personal Access Token and ADO REST API URL

--- ado_service.py
+++ ado_service.py
@@ -25,7 +25,20 @@ class AdoService:
                     raise InvalidUrlException("ADO REST API url might be incorrect.")
                 
                 return await response.json()
-            
+    
+    async def get_pull_requests(self, project_name: str, repository_name: str):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                f"{self._base_address}/{self._org_name}/{project_name}/_apis/git/repositories/{repository_name}/pullrequests?api-version=7.1-preview.1&$top=1000&searchCriteria.status=All&searchCriteria.minTime=2024-03-01T00:00:00Z&searchCriteria.maxTime=2024-09-0410T00:00:00Z", 
+                auth=self._auth) as response:
+                
+                if response.status == 203:
+                    raise InvalidPATException("Personal Access Token might be incorrect.")
+                
+                if response.status == 404:
+                    raise InvalidUrlException("ADO REST API url might be incorrect.")
+                
+                return await response.json()
             
 async def main():
     import os
@@ -39,7 +52,7 @@ async def main():
         os.getenv("ADO__PAT"))
     
     projects = await ado_service.get_projects()
-    print(projects)
+    pull_requests = await ado_service.get_pull_requests(os.getenv("ADO__PROJECT"), os.getenv("ADO__REPO"))
     
 if __name__ == "__main__":
     import asyncio
